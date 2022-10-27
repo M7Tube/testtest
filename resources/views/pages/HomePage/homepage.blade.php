e8cd538629e421cbfbd7c39391dad69aee04ce49
M7Tube <clashroyale.mahh@gmail.com>
2022-10-27 13:49:14 +0300

    jhju

--- resources/views/pages/HomePage/homepage.blade.php
+++ resources/views/pages/HomePage/homepage.blade.php
@@ -216,7 +216,7 @@
                         <li><a class="nav-link scrollto"
                                 href="{{ route('showAllPosts', app()->getLocale()) }}">{{ __('Blog') }}</a></li>
 
-                        {{-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+                        {{-- <li class="dropdown"><a href="#"><span>dDrop Down</span> <i class="bi bi-chevron-down"></i></a>
                                 <ul>
                                     <li><a href="#">Drop Down 1</a></li>
                                     <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i

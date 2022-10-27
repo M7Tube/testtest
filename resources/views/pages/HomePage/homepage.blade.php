07edb8b339347e092e8a885b1209d93d6b677277
M7Tube <clashroyale.mahh@gmail.com>
2022-10-27 13:40:07 +0300

    uyuy

--- resources/views/pages/HomePage/homepage.blade.php
+++ resources/views/pages/HomePage/homepage.blade.php
@@ -195,16 +195,16 @@
         <!-- ======= Header ======= -->
         <header id="header" class="fixed-top">
             <div class="container d-flex align-items-center justify-content-between">
-                @if (app()->getLocale() == 'ar')
+                {{-- @if (app()->getLocale() == 'ar')
                     <h1 class="logo"><a href="{{ route('homepage', app()->getLocale()) }}">{{ $info->ar_peronal_name }}</a>
                     </h1>
                 @else
                     <h1 class="logo"><a href="{{ route('homepage', app()->getLocale()) }}">{{ $info->en_peronal_name }}</a>
                     </h1>
-                @endif
+                @endif --}}
                 <!-- Uncomment below if you prefer to use an image logo -->
                 <!-- <a href="index.html" class="logo"><img src="{{ asset('img/logo.png') }}" alt="" class="img-fluid"></a>-->
-
+                <a href="index.html" class="logo"><img src="{{ asset('img/maherLogo.png') }}" alt="" class="img-fluid"></a>
                 <nav id="navbar" class="navbar">
                     <ul>
                         <li><a class="nav-link scrollto active" href="#hero">{{ __('Home') }}</a></li>
